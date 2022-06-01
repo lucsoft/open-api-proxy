31f1e8dbdd5019877c64280d30f57fff0a20edf4
lucsoft <mail@lucsoft.de>
2022-06-01 15:12:12 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,1 +1,8 @@
+import { serve } from "https://deno.land/std@0.136.0/http/server.ts";
 
+function handler(req: Request): Response {
+  return new Response("Hello world");
+}
+
+console.log("Listening on http://localhost:8000");
+await serve(handler);
